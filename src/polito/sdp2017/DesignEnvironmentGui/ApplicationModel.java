ce6a4c578f51da82f60316e5e9d28ef579075735
Emanuele Parisi <ema.par_93@yahoo.it>
2017-05-28 17:40:00 +0200

    minor changes

---
+++ src/polito/sdp2017/DesignEnvironmentGui/ApplicationModel.java
@@ -0,0 +1,29 @@
+package polito.sdp2017.DesignEnvironmentGui;
+
+import java.sql.SQLException;
+
+import polito.sdp2017.Components.DBManager;
+import polito.sdp2017.Components.SQLiteManager;
+
+public class ApplicationModel {
+	boolean databaseConnectionUp;
+	DBManager database;
+	
+	public ApplicationModel() {
+		super();
+		database = new SQLiteManager();
+		databaseConnectionUp = false;
+	}
+	
+	public boolean openDatabaseConnection(String dbPath) throws ClassNotFoundException, SQLException {
+		database.openConnection(dbPath);
+		databaseConnectionUp = true;
+		return databaseConnectionUp;
+	}
+	
+	public void closeDatabaseConnection(String dbPath) throws SQLException {
+		database.closeConnection();
+		databaseConnectionUp = false;
+	}
+
+}
