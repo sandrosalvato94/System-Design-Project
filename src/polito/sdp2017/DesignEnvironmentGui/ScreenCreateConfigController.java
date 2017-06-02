dab0db3393e2eaf74d2abbb1f56924906c876ea8
Emanuele Parisi <ema.par_93@yahoo.it>
2017-06-02 18:44:50 +0200

    create new configuration started

--- src/polito/sdp2017/DesignEnvironmentGui/ScreenCreateConfigController.java
+++ src/polito/sdp2017/DesignEnvironmentGui/ScreenCreateConfigController.java
@@ -1,9 +1,28 @@
 package polito.sdp2017.DesignEnvironmentGui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ScreenCreateConfigController implements ControlledScreen {
 	ApplicationModel applicationModel;
     ScreensController myController;
 
+    @FXML private TextField IPIdentifier;
+    @FXML private TextField IPName;
+    @FXML private TextField maxLUTs;
+    @FXML private TextField maxFFs;
+    @FXML private TextField maxLatency;
+    @FXML private TextField maxPowerConsumption;
+    @FXML private TextField maxClockFrequency;
+    @FXML private TextField contactPointId;
+    @FXML private TextField contactPointName;
+    @FXML private TextField company;    
+    @FXML private TextField physicalAddress;
+    @FXML private TextField interruptPriority;
+    @FXML private TextArea logArea;
+    
     @Override
     public void setScreenParent(ScreensController screenParent){
     	myController = screenParent;
@@ -12,5 +31,20 @@ public class ScreenCreateConfigController implements ControlledScreen {
 	@Override
 	public void setApplicationModel(ApplicationModel applicationModel) {
 		this.applicationModel = applicationModel;
+	}
+	
+	@FXML
+	public void goBack (ActionEvent event) {
+		myController.setScreen(Main.screenStartID);
+	}
+	
+	@FXML
+	public void searchIP (ActionEvent event) {
+		logArea.appendText("search\n");
+	}
+	
+	@FXML
+	public void addIPToConf (ActionEvent event) {
+		logArea.appendText("add ip to configuration\n");
 	}
 }
